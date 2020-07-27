9f67ec90773945a3d3600f069f8691d2423535a7
jumodada <864353360@qq.com>
2020-07-27 11:02:54 +0800

    chore: 路由忽视install和start

--- Docs/router/router.js
+++ Docs/router/router.js
@@ -4,16 +4,30 @@ import Content from '../components/content'
 const viewFiles = require.context('../view', true)
 const views = viewFiles.keys().reduce((views, path) => {
     const value = viewFiles(path).default
-    if (['404', '500'].indexOf(value.name) === -1) {
+    const isNotErrorPage = ['404','500'].indexOf(value.name) === -1
+    const isNotCompass = !/start|install/g.test(value.__file)
+    if (isNotErrorPage&&isNotCompass) {
         let name = value.__file.match(/view\/(\S*)\/index/)[1]
         views.push({
             path: '/components/' + name,
+            meta:{
+              type:'component'
+            },
             name,
             component: () => import(`../view/${name}/index.md`)
         })
     }
     return views
-}, [])
+}, [
+    {
+        path: '/install',
+        name: '安装',
+        meta: {
+            type:'compass'
+        },
+        component: () => import('../view/install/index.md')
+    },
+])
 
 export default [
     {
