cd42f13c698b17ea0fa4a6814315c700318c357b
jumodada <864353360@qq.com>
2020-07-27 17:12:01 +0800

    chore: update gulp config

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,14 +1,19 @@
 'use strict'
 
-const { series, src, dest } = require('gulp')
+const {series, src, dest} = require('gulp')
 const sass = require('gulp-sass')
 const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
 
 function compile() {
-  return src('../src/assets/*.scss')
-      .pipe(sass.sync())
-      .pipe(cssmin())
-      .pipe(dest('./lib'))
+    return src('../src/assets/*.scss')
+        .pipe(sass.sync())
+        .pipe(rename(function (path) {
+            path.basename =  "index"
+            path.extname = ".css"
+        }))
+        .pipe(cssmin())
+        .pipe(dest('../dist'))
 }
 
 
--- build/rollup.config.js
+++ build/rollup.config.js
@@ -13,7 +13,6 @@ const config = {
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
     include: 'src/**',
