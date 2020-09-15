334859a4b9472ac7302e4aedff53722f9f337932
jumodada <864353360@qq.com>
2020-09-15 14:04:42 +0800

    test: update typeof

--- src/utils/typeOf.test.ts
+++ src/utils/typeOf.test.ts
@@ -2,24 +2,24 @@ import {isString, isArray, isFunc, isNumber, isObject} from './typeOf'
 
 describe('type-of', () => {
   test('variable is number', () => {
-    expect(isNumber(12)).toBe(true)
-    expect(isNumber('123')).toBe(false)
-    expect(isNumber(NaN)).toBe(false)
-    expect(isNumber(Number('123'))).toBe(true)
+    expect(isNumber(12)).toBeTruthy()
+    expect(isNumber('123')).toBeFalsy()
+    expect(isNumber(NaN)).toBeFalsy()
+    expect(isNumber(Number('123'))).toBeTruthy()
   })
 
   test('variable is Array', () => {
-    expect(isArray(12)).toBe(false)
-    expect(isArray([123])).toBe(true)
-    expect(isArray(Array.from([123]))).toBe(true)
+    expect(isArray(12)).toBeFalsy()
+    expect(isArray([123])).toBeTruthy()
+    expect(isArray(Array.from([123]))).toBeTruthy()
     ;(function () {
-      expect(isArray(arguments)).toBe(false)
+      expect(isArray(arguments)).toBeFalsy()
     })()
   })
 
   test('variable is Function', () => {
-    expect(isFunc(12)).toBe(false)
-    expect(isFunc({})).toBe(false)
+    expect(isFunc(12)).toBeFalsy()
+    expect(isFunc({})).toBeFalsy()
 
     const testArrowFn = () => null
 
@@ -27,29 +27,29 @@ describe('type-of', () => {
       return null
     }
 
-    expect(isFunc(testArrowFn)).toBe(true)
+    expect(isFunc(testArrowFn)).toBeTruthy()
 
-    expect(isFunc(testFn)).toBe(true)
+    expect(isFunc(testFn)).toBeTruthy()
   })
 
   test('variable is String', () => {
-    expect(isString(12)).toBe(false)
-    expect(isString('123')).toBe(true)
-    expect(isString(Number('123'))).toBe(false)
+    expect(isString(12)).toBeFalsy()
+    expect(isString('123')).toBeTruthy()
+    expect(isString(Number('123'))).toBeFalsy()
     const testObj = {
       name: 123
     }
-    expect(isString(testObj.toString())).toBe(true)
+    expect(isString(testObj.toString())).toBeTruthy()
   })
 
   test('variable is Object', () => {
-    expect(isObject({})).toBe(true)
-    expect(isObject(Object.create(null))).toBe(true)
-    expect(isObject([])).toBe(false)
-    expect(isObject(() => null)).toBe(false)
+    expect(isObject({})).toBeTruthy()
+    expect(isObject(Object.create(null))).toBeTruthy()
+    expect(isObject([])).toBeFalsy()
+    expect(isObject(() => null)).toBeFalsy()
     const testObj = {
       name: 123
     }
-    expect(isObject(testObj.toString())).toBe(true)
+    expect(isObject(testObj)).toBeTruthy()
   })
 })
