44b11242c361c0616061d31d87d258a09f00e312
jumodada <864353360@qq.com>
2020-09-16 11:38:18 +0800

    refactor: remove getNodeName

--- src/utils/getNodeName.ts
+++
@@ -1,5 +0,0 @@
-import {Window} from '../types/windows'
-
-export function getNodeName(el: Node | Window | null): string | null {
-  return el ? (('nodeName' in el && el.nodeName) || '').toLowerCase() : null
-}
