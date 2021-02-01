794d750e4e80cca94701b87447b06a6378768622
jumodada <864353360@qq.com>
2021-02-01 11:25:51 +0800

    test: clickoutside

--- tests/utils/clickoutside.test.ts
+++ tests/utils/clickoutside.test.ts
@@ -1,31 +1,23 @@
 import clickOutside from '../../src/utils/clickoutside'
 import Better from '../../src'
 import {getState} from '../../src/store'
+import {Bind} from "../../src/utils/helper"
 
 
 describe('click-outside', () => {
   const ref = document.createElement('input')
-  const outsideNode = document.createElement('div')
   new Better(ref)
-
-  test('click input element to open popover', () => {
-    //todo
+  const state = getState()
+  document.body.addEventListener('click', Bind(clickOutside, state))
+  test('点击输入框会打开弹出框', () => {
+    expect(state.visible).toBeFalsy()
+    ref.click()
+    expect(state.visible).toBeTruthy()
   })
-  // test('click outside to close popover', () => {
-  //   const rec = {
-  //     components: {
-  //       reference: get('reference'),
-  //       popover: get('popover')
-  //     },
-  //     utils: {
-  //       visible: get('visible')
-  //     }
-  //   }
-  //   const mockEvent = {
-  //     target: outsideNode
-  //   }
-  //   expect(get('visible')).toBeTruthy()
-  //   clickOutside(rec as any, mockEvent)
-  //   expect(get('visible')).toBeFalsy()
-  // })
+  test('点击外部会关闭弹出框', () => {
+    ref.click()
+    expect(state.visible).toBeTruthy()
+    document.body.click()
+    expect(state.visible).toBeFalsy()
+  })
 })
