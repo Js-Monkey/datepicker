87fb4e5b8791a22029c10913f820491a8e3c830d
jumodada <864353360@qq.com>
2021-03-09 13:50:24 +0800

    chore: i18n en

--- src/core/util/i18n.ts
+++ src/core/util/i18n.ts
@@ -1,15 +1,15 @@
-export const dayBarNames = ['日', '一', '二', '三', '四', '五', '六']
+export const dayBarNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
 export const monthNames = [
-  '一月',
-  '二月',
-  '三月',
-  '四月',
-  '五月',
-  '六月',
-  '七月',
-  '八月',
-  '九月',
-  '十月',
-  '十一月',
-  '十二月'
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
 ]
