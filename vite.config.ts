98c63f5e2b74cf2baf6da63100a25b05cb1fcafb
xipper <864353360@qq.com>
2021-05-28 23:48:22 +0800

    build: split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ export default {
   server: {
     port: 8848
   },
+  build: {
+    rollupOptions: {
+      output:{
+        manualChunks(id: any) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/]
