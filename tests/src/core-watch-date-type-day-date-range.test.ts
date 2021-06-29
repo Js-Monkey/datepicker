26a515c8fc4ab6c4c0d1029c089de046c1a10ed8
wukunyun <864353360@qq.com>
2021-06-29 23:11:16 +0800

    test: update date-range

--- tests/src/core-watch-date-type-day-date-range.test.ts
+++ tests/src/core-watch-date-type-day-date-range.test.ts
@@ -1,25 +1,7 @@
 import {hoverSelect} from "../../src/core/watch/date/type/public"
 import {createState} from "../../src/store"
 import defaultOptions from "../../src/core/util/default-options"
-
-// describe('updateRangeStartMonth', () => {
-//   const state = createState(defaultOptions())
-//   it('should changed `startMonth` and `startYear`', () => {
-//     rangeLinkPrecisionToDay.cb.call(state, 1, 2021)
-//     expect(state.start.year).toBe(2020)
-//     expect(state.start.month).toBe(12)
-//   });
-// })
-
-
-// describe('updateRangeEndMonth', () => {
-//   const state = createState(defaultOptions())
-//   it('should changed `endMonth` and `endYear`', () => {
-//     startLinkEndToMonth.cb.call(state, 12, 2000)
-//     expect(state.end.year).toBe(2001)
-//     expect(state.end.month).toBe(1)
-//   });
-// })
+import {linkMonth} from "../../src/core/watch/date/type/date&week/date-range"
 
 
 describe('hoverDay callback', () => {
@@ -27,5 +9,42 @@ describe('hoverDay callback', () => {
     it('should changed all `_date.status`', () => {
         hoverSelect().cb.call(state)
         expect(state.start._date[0].status).toBe('')
+    });
+})
+
+
+describe('linkMonth', () => {
+    const state = createState(defaultOptions())
+    it('should change `end.month` and `end.year`', () => {
+        const mockState = {
+            start: {
+                month: 9,
+                year: 2021
+            },
+            end: {
+                month: 10,
+                year: 2021
+            }
+        } as any
+        const {cb} = linkMonth()
+        cb.call(mockState, 11, 2021)
+        expect(mockState.end.month).toBe(12)
+        expect(mockState.end.year).toBe(2021)
+    });
+    it('should change `start.month` and `start.year`', () => {
+        const mockState = {
+            start: {
+                month: 9,
+                year: 2021
+            },
+            end: {
+                month: 10,
+                year: 2021
+            }
+        } as any
+        const {cb} = linkMonth('end')
+        cb.call(mockState, 1, 2021)
+        expect(mockState.start.month).toBe(12)
+        expect(mockState.start.year).toBe(2020)
     });
 })
